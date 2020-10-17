b77719053568c4ab99aa899ed757698433c44c8a
lie16 <yonattan_hope@yahoo.com>
2020-10-17 14:48:06 +0800

    convert to statefullwidget,  adding controller, ui

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_arc_by_package_creator/core/viemodels/login_view_models.dart';
 import 'package:provider_arc_by_package_creator/ui/shared/app_colors.dart';
+import 'package:provider_arc_by_package_creator/ui/widgets/login_header.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<LoginViewModel>.value(
@@ -13,7 +20,22 @@ class LoginView extends StatelessWidget {
       child: Consumer<LoginViewModel>(
         builder: (context, model, child) => Scaffold(
             backgroundColor: backgroundColor,
-            body: Center(child: Text('Login View'))),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LoginHeader(controller: _controller),
+                model.busy
+                    ? CircularProgressIndicator()
+                    : FlatButton(
+                        color: Colors.white,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {},
+                      )
+              ],
+            )),
       ),
     );
   }
