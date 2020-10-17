c579f05e686148c741c26529cd5eb0ae18bf723b
lie16 <yonattan_hope@yahoo.com>
2020-10-17 11:33:00 +0800

    add changeNotifierProvider & Consumer to LoginView

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_arc_by_package_creator/core/viemodels/login_view_models.dart';
 import 'package:provider_arc_by_package_creator/ui/shared/app_colors.dart';
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: backgroundColor,
-        body: Center(child: Text('Login View')));
+    return ChangeNotifierProvider<LoginViewModel>.value(
+      value: LoginViewModel(),
+      child: Consumer<LoginViewModel>(
+        builder: (context, model, child) => Scaffold(
+            backgroundColor: backgroundColor,
+            body: Center(child: Text('Login View'))),
+      ),
+    );
   }
 }
