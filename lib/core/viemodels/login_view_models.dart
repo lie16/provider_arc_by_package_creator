c1845014365e744c793df06ecd135259387f331d
lie16 <yonattan_hope@yahoo.com>
2020-10-17 11:29:19 +0800

    add login_view_models and bacis login function

---
+++ lib/core/viemodels/login_view_models.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:provider_arc_by_package_creator/core/services/authentication_services.dart';
+
+class LoginViewModel extends ChangeNotifier {
+  AuthenticationService _authenticationService;
+
+  LoginViewModel({@required AuthenticationService authenticationService})
+      : _authenticationService = authenticationService;
+
+  Future<bool> login(String userIdText) async {
+    setBusy(true);
+    var userId = int.tryParse(userIdText);
+    var success = await _authenticationService.login(userId);
+    setBusy(false);
+    return success;
+  }
+
+  bool _busy = false;
+  bool get busy => _busy;
+
+  void setBusy(bool value) {
+    _busy = value;
+    notifyListeners();
+  }
+}
