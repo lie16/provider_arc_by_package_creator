8599f161978da05da9e69e54f96f9c5974d899c5
lie16 <yonattan_hope@yahoo.com>
2020-10-17 10:33:58 +0800

    Inject provider to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:provider_arc_by_package_creator/core/constant/app_constant.dart';
 import 'package:provider_arc_by_package_creator/ui/router.dart';
 
@@ -7,13 +8,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    //Inject provider to app
+    return MultiProvider(
+          providers: [],
+          child: MaterialApp(
+        title: 'Provider Arc',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: RoutePaths.Login,
+        onGenerateRoute: Router.generateRoute,
       ),
-      initialRoute: RoutePaths.Login,
-      onGenerateRoute: Router.generateRoute,
     );
   }
 }
