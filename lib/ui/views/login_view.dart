e7f6b92ff15d7082cc4077aa70c40f10a6e581b0
lie16 <yonattan_hope@yahoo.com>
2020-10-17 15:38:22 +0800

    replace ChangeNotifierProvider with base_widget

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_arc_by_package_creator/core/constant/app_constant.dart';
 import 'package:provider_arc_by_package_creator/core/viemodels/views/login_view_models.dart';
+import 'package:provider_arc_by_package_creator/core/viemodels/widgets/base_widget.dart';
 import 'package:provider_arc_by_package_creator/ui/shared/app_colors.dart';
 import 'package:provider_arc_by_package_creator/ui/widgets/login_header.dart';
 
@@ -14,36 +15,38 @@ class _LoginViewState extends State<LoginView> {
   final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<LoginViewModel>.value(
-      value: LoginViewModel(
-          // Inject authentication service setup in the provider_setup
-          authenticationService: Provider.of(context)),
-      child: Consumer<LoginViewModel>(
-        builder: (context, model, child) => Scaffold(
-            backgroundColor: backgroundColor,
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                LoginHeader(controller: _controller),
-                model.busy
-                    ? CircularProgressIndicator()
-                    : FlatButton(
-                        color: Colors.white,
-                        child: Text(
-                          'Login',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        onPressed: () async {
-                          var loginSuccess =
-                              await model.login(_controller.text);
-                          if (loginSuccess) {
-                            Navigator.pushNamed(context, RoutePaths.Home);
-                          }
-                        },
-                      )
-              ],
-            )),
-      ),
+    // return ChangeNotifierProvider<LoginViewModel>.value(
+    // value: LoginViewModel(
+    // Inject authentication service setup in the provider_setup
+    //authenticationService: Provider.of(context)),
+    //child: Consumer<LoginViewModel>(
+    return BaseWidget<LoginViewModel>(
+      //replacing above code
+      model: LoginViewModel(authenticationService: Provider.of(context)),
+      // pass the LogineHeader as a prebuilt-static child/dalam child = tidak rebuild
+      child: LoginHeader(controller: _controller),
+      builder: (context, model, child) => Scaffold(
+          backgroundColor: backgroundColor,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              model.busy
+                  ? CircularProgressIndicator()
+                  : FlatButton(
+                      color: Colors.white,
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () async {
+                        var loginSuccess = await model.login(_controller.text);
+                        if (loginSuccess) {
+                          Navigator.pushNamed(context, RoutePaths.Home);
+                        }
+                      },
+                    )
+            ],
+          )),
     );
   }
 }
