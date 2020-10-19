09e0a99bbd318f8efe199585a961972fb92b914d
lie16 <yonattan_hope@yahoo.com>
2020-10-19 16:09:50 +0800

    home_view.dart

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_arc_by_package_creator/core/models/user.dart';
 import 'package:provider_arc_by_package_creator/ui/shared/app_colors.dart';
+import 'package:provider_arc_by_package_creator/ui/shared/text_styles.dart';
+import 'package:provider_arc_by_package_creator/ui/shared/ui_helpers.dart';
+import 'package:provider_arc_by_package_creator/ui/widgets/posts.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: backgroundColor,
-        body: Center(child: Text('Home View')));
+      backgroundColor: backgroundColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          UIHelper.verticalSpaceLarge,
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Text(
+              // consume UserStream
+              'Welcome ${Provider.of<User>(context).name}',
+              style: headerStyle,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Text('Here are all your posts', style: subHeaderStyle),
+          ),
+          UIHelper.verticalSpaceSmall,
+          Expanded(
+            child: Posts(),
+          )
+        ],
+      ),
+    );
   }
 }
