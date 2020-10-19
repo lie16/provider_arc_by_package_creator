a5a28f00421edf5421cac6554bc8861d9967ef3a
lie16 <yonattan_hope@yahoo.com>
2020-10-19 16:00:03 +0800

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_arc_by_package_creator/core/constant/app_constant.dart';
+
 import 'package:provider_arc_by_package_creator/provider_setup.dart';
 import 'package:provider_arc_by_package_creator/ui/router.dart';
+
+import 'package:provider_arc_by_package_creator/core/constant/app_constant.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,12 +15,12 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: providers,
       child: MaterialApp(
-        title: 'Provider Arc',
+        title: 'Provider Architecture',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         initialRoute: RoutePaths.Login,
-        onGenerateRoute: Router.generateRoute,
+        onGenerateRoute: Routers.generateRoute,
       ),
     );
   }
