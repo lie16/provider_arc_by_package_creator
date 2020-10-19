a723ed397498976997953dabe4159ac8eed3b658
lie16 <yonattan_hope@yahoo.com>
2020-10-19 16:15:42 +0800

    updating post_view.dart

--- lib/ui/views/post_view.dart
+++ lib/ui/views/post_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:provider_arc_by_package_creator/core/models/post.dart';
+import 'package:provider_arc_by_package_creator/core/models/user.dart';
 import 'package:provider_arc_by_package_creator/ui/shared/app_colors.dart';
+import 'package:provider_arc_by_package_creator/ui/shared/text_styles.dart';
+import 'package:provider_arc_by_package_creator/ui/shared/ui_helpers.dart';
+import 'package:provider_arc_by_package_creator/ui/widgets/comments.dart';
 
 class PostView extends StatelessWidget {
   final Post post;
@@ -9,7 +14,24 @@ class PostView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: backgroundColor,
-        body: Center(child: Text('Post View')));
+      backgroundColor: backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            UIHelper.verticalSpaceLarge,
+            Text(post.title, style: headerStyle),
+            Text(
+              'by ${Provider.of<User>(context).name}',
+              style: TextStyle(fontSize: 9.0),
+            ),
+            UIHelper.verticalSpaceMedium,
+            Text(post.body),
+            Comments(post.id)
+          ],
+        ),
+      ),
+    );
   }
 }
